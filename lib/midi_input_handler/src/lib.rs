0be84c927c001b8406d0068a3bc0cd9624de5ac3
“CorentinVaillant” <c.vaillant848@gmail.com>
2024-05-01 20:17:44 +0200

    finish writting code let's test !

--- lib/midi_input_handler/src/lib.rs
+++ lib/midi_input_handler/src/lib.rs
@@ -1,21 +1,40 @@
 mod midi_handler;
 
+use std::thread;
+
 use midi_handler::*;
 use mlua::prelude::*;
 
+static mut BUFFER: Vec<MidiInputPressed> = vec![];
+
+//LUA functions
 
 //same name as the lib (path include...)
-#[mlua::lua_module] fn libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {lua_init(lua)}
-#[mlua::lua_module] fn target_debug_libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {lua_init(lua)}
-#[mlua::lua_module] fn libmidi_input_handler_libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {lua_init(lua)}
+#[mlua::lua_module]
+fn libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {
+    lua_init(lua)
+}
+#[mlua::lua_module]
+fn target_debug_libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {
+    lua_init(lua)
+}
+#[mlua::lua_module]
+fn libmidi_input_handler_libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {
+    lua_init(lua)
+}
 
-#[mlua::lua_module] fn lib_midi_input_handler_libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {lua_init(lua)}
+#[mlua::lua_module]
+fn lib_midi_input_handler_libmidi_input_handler(lua: &Lua) -> LuaResult<LuaTable> {
+    lua_init(lua)
+}
 
 #[mlua::lua_module]
 fn lua_init(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("print_rust", lua.create_function(lua_print_rust)?)?;
     exports.set("init_midi", lua.create_function(lua_init_midi)?)?;
+    exports.set("get_inputs", lua.create_function(lua_get_inputs)?)?;
+
     Ok(exports)
 }
 
@@ -25,23 +44,73 @@ fn lua_print_rust(_: &Lua, message: String) -> LuaResult<()> {
     Ok(())
 }
 
-//function that I want to use
-fn lua_init_midi(_: &Lua,table :LuaTable) -> LuaResult<()> {
-    init();
+fn lua_init_midi(lua: &Lua, _: ()) -> LuaResult<LuaTable> {
+    let input_table = lua.create_table()?;
+    thread::spawn(|| {
+        let receiver = init();
+        loop {
+            let input = receiver.recv();
+            unsafe {
+                BUFFER.push(input.unwrap()); //TODO handle unwrap
+            }
+        }
+    });
 
-    Ok(())
+    Ok(input_table)
 }
 
+//renvoie None ou les inputs d'un buffer
+fn lua_get_inputs(lua: &Lua, _: ()) -> LuaResult<LuaTable> {
+    let buffer_table = lua.create_table()?;
+    print!("{}", unsafe { BUFFER.len() });
+    for (i, input) in unsafe { BUFFER.iter().enumerate() } {
+        buffer_table.set(i, midi_input_to_table(lua, input)?)?;
+    }
+    println!(" -> {}", unsafe { BUFFER.len() });
+    Ok(buffer_table)
+}
 
-//table to informe that a button has a value
-fn MidiInputPressedToTable(input : MidiInputPressed,lua: &Lua)->LuaResult<LuaTable>{
-    let table = lua.create_table()?;
+//---------------------
 
-    
-    //grrrr fonctionne pas
-    //TODO
+fn midi_input_to_table<'a>(lua: &'a Lua, input: &'a MidiInputPressed) -> LuaResult<LuaTable<'a>> {
+    let input_table= lua.create_table()?;
+    match input {
+        MidiInputPressed::Note(note) => {
+            input_table.set("midi_type", "note")?;
+            input_table.set("oct", note.oct)?;
+            input_table.set("note", note.note)?;
+            input_table.set("velocity", note.velocity)?;
+            input_table.set("channel", note.get_channel_num())?;
+        }
+        MidiInputPressed::JoystickX(midi_val) => {
+            midival_into_table(&input_table, midi_val, "JoystickX".to_string())?
+        }
+        MidiInputPressed::JoystickY(midi_val) => {
+            midival_into_table(&input_table, midi_val, "JoystickY".to_string())?
+        }
+        MidiInputPressed::Knob(midi_val) => {
+            midival_into_table(&input_table, midi_val, "Knob".to_string())?
+        }
+        MidiInputPressed::Unknow(midi_val) => {
+            input_table.set("midi_type", "unknown")?;
+            input_table.set("id", *midi_val)?;
+        }
 
-    
-    
-    Ok(table)
+        MidiInputPressed::None => (),
+    }
+
+    Ok(input_table)
+}
+
+fn midival_into_table(
+    input_table: &LuaTable,
+    midi_val: &MidiValue,
+    midi_type: String,
+) -> LuaResult<()> {
+    input_table.set("midi_type", midi_type)?;
+    input_table.set("value", midi_val.value)?;
+    input_table.set("key", midi_val.key)?;
+    input_table.set("channel", midi_val.get_channel_num())?;
+
+    Ok(())
 }
--- lib/midi_input_handler/src/midi_handler.rs
+++ lib/midi_input_handler/src/midi_handler.rs
@@ -1,21 +1,22 @@
-
 use midi_control::*;
 use midir::*;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 
-
 #[allow(dead_code)]
-pub const FRENCH_NOTE_TABLE : [&str;12] = ["do" ,"do#","re","re#","mi","fa","fa#","sol","sol#","la","la#","si"];
-pub const ENGLISH_NOTE_TABLE : [&str;12]= ["C" , "C#", "D", "D#", "E", "F", "F#", "F" , "F#" , "A", "A#", "B" ];
+pub const FRENCH_NOTE_TABLE: [&str; 12] = [
+    "do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si",
+];
+pub const ENGLISH_NOTE_TABLE: [&str; 12] = [
+    "C", "C#", "D", "D#", "E", "F", "F#", "F", "F#", "A", "A#", "B",
+];
 
-#[allow(dead_code)]
 pub struct Note {
-    oct: u8,
-    note: u8,
-    velocity: u8,
+    pub oct: u8,
+    pub note: u8,
+    pub velocity: u8,
 
-    channel: Channel,
+    pub channel: Channel,
 }
 impl Note {
     pub fn from(key: KeyEvent, channel: Channel) -> Self {
@@ -27,122 +28,198 @@ impl Note {
         }
     }
 
-    fn to_string(&self) -> String{
-        format!("{}-{}",ENGLISH_NOTE_TABLE[usize::from(self.note)],self.oct)
+    fn to_string(&self) -> String {
+        format!(
+            "{}-{}",
+            ENGLISH_NOTE_TABLE[usize::from(self.note)],
+            self.oct
+        )
     }
-    
+    #[allow(dead_code)]
+    pub fn get_channel_num(&self) -> u8 {
+        match self.channel {
+            Channel::Ch1 => 1,
+            Channel::Ch2 => 2,
+            Channel::Ch3 => 3,
+            Channel::Ch4 => 4,
+            Channel::Ch5 => 5,
+            Channel::Ch6 => 6,
+            Channel::Ch7 => 7,
+            Channel::Ch8 => 8,
+            Channel::Ch9 => 9,
+            Channel::Ch10 => 10,
+            Channel::Ch11 => 11,
+            Channel::Ch12 => 12,
+            Channel::Ch13 => 13,
+            Channel::Ch14 => 14,
+            Channel::Ch15 => 15,
+            Channel::Ch16 => 16,
+            Channel::Invalid => 0,
+        }
+    }
 }
 
 #[allow(dead_code)]
-pub struct MidiValue{
-    value : i16,
-    key :u8,
-    channel : Channel
+pub struct MidiValue {
+    pub value: i16,
+    pub key: u8,
+    pub channel: Channel,
 }
 impl MidiValue {
-    pub fn to_string(&self) ->String{
-        format!("{}-{}-{:?}",self.value,self.key,self.channel)
+    #[allow(dead_code)]
+    pub fn to_string(&self) -> String {
+        format!("{}-{}-{:?}", self.value, self.key, self.channel)
+    }
+    #[allow(dead_code)]
+    pub fn get_channel_num(&self) -> u8 {
+        match self.channel {
+            Channel::Ch1 => 1,
+            Channel::Ch2 => 2,
+            Channel::Ch3 => 3,
+            Channel::Ch4 => 4,
+            Channel::Ch5 => 5,
+            Channel::Ch6 => 6,
+            Channel::Ch7 => 7,
+            Channel::Ch8 => 8,
+            Channel::Ch9 => 9,
+            Channel::Ch10 => 10,
+            Channel::Ch11 => 11,
+            Channel::Ch12 => 12,
+            Channel::Ch13 => 13,
+            Channel::Ch14 => 14,
+            Channel::Ch15 => 15,
+            Channel::Ch16 => 16,
+            Channel::Invalid => 0,
+        }
     }
 }
+
+#[allow(dead_code)]
 
 pub enum MidiInputPressed {
     Note(Note),
     JoystickX(MidiValue),
     JoystickY(MidiValue),
     Knob(MidiValue),
-    None
+    Unknow(u16),
+    None,
 }
 
 impl MidiInputPressed {
-    pub fn get_input_name(&self)->String{
+    #[allow(dead_code)]
+    pub fn get_input_name(&self) -> String {
         match self {
-            Self::None=>"None".to_string(),
+            Self::None => "None".to_string(),
             Self::Note(note) => note.to_string(),
             Self::JoystickX(val) => val.to_string(),
             Self::JoystickY(val) => val.to_string(),
-            Self::Knob(val) => val.to_string()
-        
+            Self::Knob(val) => val.to_string(),
+            Self::Unknow(val) => format!("{:x}", val),
         }
     }
 }
 
-
-
-
-
-fn callback(_timestamp: u64, data: &[u8], sender: &Sender<MidiInputPressed>) {
+fn callback(_timestamp: u64, data: &[u8], sender: &mut Sender<MidiInputPressed>) {
     let message = MidiMessage::from(data);
     print!("\nreceived midi data {:?} -> ", data);
     match message {
         MidiMessage::NoteOn(channel, key) => {
             let note: Note = Note::from(key, channel);
-            #[cfg(debug_assertions)] println!("{} on channel : {channel:?} ", note.to_string());
-            //use channel
+            #[cfg(debug_assertions)]
+            println!("{} on channel : {channel:?} ", note.to_string());
         }
         MidiMessage::NoteOff(channel, key) => {
             let note: Note = Note::from(key, channel);
-            #[cfg(debug_assertions)] println!("{} on channel : {channel:?} ", note.to_string());
-            //use channel
+            #[cfg(debug_assertions)]
+            println!("{} on channel : {channel:?} ", note.to_string());
+            sender.send(MidiInputPressed::Note(note)).unwrap(); //TODO HANDLE UNWRAP !!
         }
 
-        MidiMessage::PitchBend(channel,x ,y ) =>{
-            #[cfg(debug_assertions)] println!("pitch bend : ({x},{y}) on channel : {channel:?}");
-            let axis =MidiValue{ value:-1*i16::from(y)*2,key:0, channel};
-            //use channel
-
+        MidiMessage::PitchBend(channel, x, y) => {
+            #[cfg(debug_assertions)]
+            println!("pitch bend : ({x},{y}) on channel : {channel:?}");
+            let axis = MidiValue {
+                value: -1 * i16::from(y) * 2,
+                key: 0,
+                channel,
+            };
+            sender.send(MidiInputPressed::JoystickX(axis)).unwrap(); //TODO HANDLE UNWRAP !!
         }
 
-        MidiMessage::PolyKeyPressure(channel,key ) =>{
-            #[cfg(debug_assertions)] println!("polykey pressure : ({:?}) on channel : {channel:?}",key.key);
-            let axis =MidiValue{ value:i16::from(key.value),key:key.key , channel};
-            //use channel
+        MidiMessage::PolyKeyPressure(channel, key) => {
+            #[cfg(debug_assertions)]
+            println!(
+                "polykey pressure : ({:?}) on channel : {channel:?}",
+                key.key
+            );
+            let axis = MidiValue {
+                value: i16::from(key.value),
+                key: key.key,
+                channel,
+            };
+            sender.send(MidiInputPressed::JoystickX(axis)).unwrap(); //TODO HANDLE UNWRAP !!
         }
 
-        MidiMessage::ControlChange(channel,controle ) => {
-            #[cfg(debug_assertions)] println!("control change : ({:?}) -> ({:?}) on channel : {channel:?}",controle.control,controle.value);
-            let knob =MidiValue{ value:i16::from(controle.value),key:controle.control, channel};
-            //use channel
+        MidiMessage::ControlChange(channel, controle) => {
+            #[cfg(debug_assertions)]
+            println!(
+                "control change : ({:?}) -> ({:?}) on channel : {channel:?}",
+                controle.control, controle.value
+            );
+            let knob = MidiValue {
+                value: i16::from(controle.value),
+                key: controle.control,
+                channel,
+            };
+            sender.send(MidiInputPressed::Knob(knob)).unwrap(); //TODO HANDLE UNWRAP !!
         }
-        
 
-        _ => println!("unknow message received !"),
-
+        _ => {
+            #[cfg(debug_assertions)]
+            println!("unknow message received ! data : {:?}", data);
+            let mut val: u16 = 0;
+            for (i, n) in (0_u8..).zip(data.iter()) {
+                val += u16::from(*n) << (8 * i);
+            }
+            sender.send(MidiInputPressed::Unknow(val)).unwrap(); //TODO HANDLE UNWRAP !!
+        }
     }
 }
 
-pub fn init()->Receiver<MidiInputPressed> {
+pub fn init() -> Receiver<MidiInputPressed> {
     //initialisation
 
     let (sender, receiver) = channel::<MidiInputPressed>();
-thread::spawn(||{
-    let midi_input: MidiInput = match MidiInput::new("input") {
-        Ok(result) => result,
-        Err(e) => panic!("{}", e),
-    };
+    thread::spawn(move || {
+        let midi_input: MidiInput = match MidiInput::new("input") {
+            Ok(result) => result,
+            Err(e) => panic!("{}", e),
+        };
 
+        //conection
+        let ports_nb = midi_input.port_count();
+        println!("{} ports avalaibles", ports_nb);
+        let _connection_number = 0;
 
-    //conection
-    let ports_nb = midi_input.port_count();
-    println!("{} ports avalaibles", ports_nb);
-    let _connection_number = 0;
+        let mut connections: Vec<MidiInputConnection<Sender<MidiInputPressed>>> =
+            init_connections(&midi_input, &sender);
 
-    let mut connections: Vec<MidiInputConnection<()>> = init_connections(&midi_input,&sender);
-
-    loop {
-        if connections.len() != midi_input.port_count() {
-            connections = init_connections(&midi_input,&sender);
+        loop {
+            if connections.len() != midi_input.port_count() {
+                connections = init_connections(&midi_input, &sender);
+            }
         }
-    }
-    
-});
+    });
     receiver
 }
 
-fn init_connections<'a>(midi_input: & MidiInput,sender: &Sender<MidiInputPressed>) -> Vec<MidiInputConnection<()>> {
-    let mut return_vec: Vec<MidiInputConnection<()>> = vec![];
+fn init_connections(
+    midi_input: &MidiInput,
+    sender: &Sender<MidiInputPressed>,
+) -> Vec<MidiInputConnection<Sender<MidiInputPressed>>> {
+    let mut return_vec: Vec<MidiInputConnection<Sender<MidiInputPressed>>> = vec![];
 
     for port in midi_input.ports() {
-
         let port_name = midi_input
             .port_name(&port)
             .expect("Error getting port name");
@@ -150,7 +227,7 @@ fn init_connections<'a>(midi_input: & MidiInput,sender: &Sender<MidiInputPressed
 
         match MidiInput::new(&format!("conection {port_name}"))
             .expect("error new midi input")
-            .connect(&port, name, callback,  &sender)
+            .connect(&port, name, callback, sender.clone())
         {
             Ok(result) => return_vec.push(result),
             Err(e) => eprintln!("Error connecting to port {}: {:?}", name, e),
